75fd84111ce466e9df7d80eda45a74518ac347f8
affanrk <affan.kurniadi@gmail.com>
2024-11-13 00:58:59 +0700

    for deploy

--- build.sh
+++ build.sh
@@ -1,4 +1,18 @@
-pip3.12 install --disable-pip-version-check --target
-pip --upgrade -r /vercel/path0/requirements.txt
+#!/bin/bash
+
+# Install SDL2 dependencies
+apt-get update && apt-get install -y \
+    libsdl2-dev \
+    libsdl2-image-dev \
+    libsdl2-mixer-dev \
+    libsdl2-ttf-dev \
+    libportmidi-dev \
+    libswscale-dev \
+    libsmpeg-dev \
+    libavformat-dev \
+    libavcodec-dev \
+    libfreetype6-dev \
+    libx11-6
+
+# Install Python dependencies
 pip install -r requirements.txt
-apt-get install build-essentials
