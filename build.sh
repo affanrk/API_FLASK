2bd3c76271c4a46390ae3b3915905c82336f4b4d
affanrk <affan.kurniadi@gmail.com>
2024-11-13 01:05:49 +0700

    for deploy

--- build.sh
+++ build.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 
-# Install SDL2 dependencies
+# Install necessary system dependencies
 apt-get update && apt-get install -y \
-    libsdl2-dev \
-    libsdl2-image-dev \
-    libsdl2-mixer-dev \
-    libsdl2-ttf-dev \
-    libportmidi-dev \
-    libswscale-dev \
-    libsmpeg-dev \
-    libavformat-dev \
-    libavcodec-dev \
-    libfreetype6-dev \
-    libx11-6
+    libgl1-mesa-glx \
+    libglib2.0-0
 
 # Install Python dependencies
 pip install -r requirements.txt
