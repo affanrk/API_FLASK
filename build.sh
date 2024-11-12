41f9ea30be2642a45ed8a29ec3a47aa27d7f6991
affanrk <affan.kurniadi@gmail.com>
2024-11-13 06:43:05 +0700

    for deploy

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Install necessary system dependencies
-apt-get update && apt-get install -y \
-    libgl1-mesa-glx \
-    libglib2.0-0
-
-# Install Python dependencies
-pip install -r requirements.txt
